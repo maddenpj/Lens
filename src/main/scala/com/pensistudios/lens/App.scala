3a665bbea7c61f62edca2442eb86998e55053c66
Patrick Madden <maddenpj@gmail.com>
2013-09-19 23:36:37 -0500

    Add circle stuff

--- src/main/scala/com/pensistudios/lens/App.scala
+++ src/main/scala/com/pensistudios/lens/App.scala
@@ -1,6 +1,7 @@
 package com.pensistudios.lens;
 
 import scala.math._
+import util.Random
 
 import org.lwjgl.LWJGLException
 import org.lwjgl.opengl.{Display, DisplayMode, GL11}
@@ -37,13 +38,13 @@ object App {
       val dt = (getTime - lastFrame).toInt
       lastFrame = getTime
 
-      T += dt 
+      T += dt
 
       val res = 3
       for(i <- 0 to(800, res) ) {
         for(j <- 0 to(600, res) ) {
           val pos = Point(i, j)
-          drawRect(pos, Point(res, res), colorThree(pos, T))
+          drawRect(pos, Point(res, res), colorSignal(pos, T))
         }
       }
 
@@ -58,14 +59,51 @@ object App {
     var b = -sin(t + 2*Pi*(pos.y/800.0)).toFloat
 
     Color(r, 0.1f, b, 1.0f)
-    
+  }
+
+  def colorSignal(pos: Point, dt: Int): Color = {
+    var x = pos.x
+    var y = pos.y
+    if(onCircle(pos, dt)) {
+      x += Random.nextInt(20) - 10
+      y += Random.nextInt(20) - 10
+    }
+    var r = cos(dt/500.0).toFloat*sin(16*Pi*(x/800.0)).toFloat + cos(8*Pi*(y/800.0)).toFloat
+    var b = -cos(dt/200.0).toFloat*sin(16*Pi*(y/600.0)).toFloat + cos(8*Pi*(x/600.0)).toFloat
+
+    var g = cos(dt/500.0).toFloat*(r-b)
+
+    if(onCircle(pos, dt)) {
+      var colorChange = -cos(dt/600.0).toFloat
+      r += colorChange
+      g -= colorChange
+      b -= colorChange + (Random.nextFloat - 1.0f)
+    }
+
+    Color(r, g, b, 1.0f)
+  }
+
+  def onCircle(pos: Point, dt: Int): Boolean = {
+    val center = Point(WINDOW_BOUNDS.x/2, WINDOW_BOUNDS.y/2)
+    /*val radius = 400*tan(dt/600.0)*/
+    val radius = 200
+    val threshold = 200*cos(dt/300.0)
+
+    val r = Point(pos.x - center.x, pos.y - center.y)
+    val mag = Math.sqrt(r.x*r.x + r.y*r.y)
+
+    (mag < radius + threshold) && (mag > radius - threshold)
   }
 
   def colorThree(pos: Point, dt: Int): Color = {
+    var rt = 16.0f*cos(dt/600.0)
+    var bt = 16.0f*cos(dt/1200.0)
+
     var r = cos(dt/500.0).toFloat*sin(16*Pi*(pos.x/800.0)).toFloat + cos(8*Pi*(pos.y/800.0)).toFloat
     var b = -cos(dt/200.0).toFloat*sin(16*Pi*(pos.y/600.0)).toFloat + cos(8*Pi*(pos.x/600.0)).toFloat
 
     var g = cos(dt/500.0).toFloat*(r-b)
+    /*var g = tan(dt*pos.x*pos.y/200.0).toFloat*(r-b)*/
 
     Color(r, g, b, 1.0f)
   }
